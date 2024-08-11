1685d246f246ac2d9d7d1f08c9eaba4d9ee0ba12
Yuting <yuting.yh@gmail.com>
2024-08-11 14:09:36 -0400

    finished login that adds/removes numbers/ops

--- calculator/script.js
+++ calculator/script.js
@@ -1,5 +1,40 @@
-//operational functions
+const display = document.querySelector("#display");
 
+const pad = document.querySelector("#pad");
+
+//store each number or operator in this array
+let equation = [];
+
+//pad listenes to any click events
+pad.addEventListener("click", (e) => {
+  //Check if button click
+  if (e.target.tagName.toLowerCase() == "button") {
+    let btnTxt = e.target.innerText;
+
+    switch (btnTxt) {
+      case "cls":
+        equation = [];
+        break;
+
+      case "<-":
+        equation.pop();
+        break;
+
+      case "%":
+        percentify();
+        break;
+
+      default:
+        equation.push(e.target.innerText);
+        break;
+    }
+  }
+
+  //after handling user input,
+  updateDisplay();
+});
+
+//operational functions
 function add(a, b) {
   return a + b;
 }
@@ -32,3 +67,16 @@ function operate(a, op, b) {
       break;
   }
 }
+
+/**
+ * Updates the HTML number display
+ */
+function updateDisplay() {
+  display.innerText = equation;
+}
+
+/**
+ * Divides the last number in equation[]
+ * if not a numebr, aleart a err message
+ */
+function percentify() {}
