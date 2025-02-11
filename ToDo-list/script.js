a2ac5d5681e5f0361ada4ab6afcd3b838b5acff4
Yuting <yuting.yh@gmail.com>
2025-02-11 01:11:38 -0500

    add new note works

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,6 +1,22 @@
+const notePad = document.getElementById("note-pad");
+
 const newNoteBtn = document.getElementById("new-note-btn");
+
+newNoteBtn.addEventListener("click", () => {
+  newEmptyNote();
+});
 
 function newEmptyNote() {
   const note = document.createElement("div");
   note.classList.add("note");
+
+  const text = "default text";
+
+  note.innerText = text;
+
+  notePad.appendChild(note);
+
+  return { note, text };
 }
+
+const myNote = newEmptyNote();
